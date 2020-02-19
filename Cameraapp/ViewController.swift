a7d62e6c828ccc4649e18d08733433a4ffee8c9c
中條航紀 <ck227200@gmail.com>
2020-02-19 16:40:55 +0900

    Initial Commit

---
+++ Cameraapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cameraapp
+//
+//  Created by 中條航紀 on 2020/02/19.
+//  Copyright © 2020 中條航紀. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
